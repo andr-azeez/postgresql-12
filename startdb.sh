4c7c3dbeada56c2a8937d92f66eb5a3812bbf8b1
andr-azeez <81161330+andr-azeez@users.noreply.github.com>
2021-07-14 13:34:20 +0530

    update comment startdb.sh

--- startdb.sh
+++ startdb.sh
@@ -2,10 +2,10 @@
 
 
 y=$(eval "ls /var/lib/postgresql/12/main | wc -l")
-echo "$y"
+#echo "$y"
 
 if [ "$y" -eq 0 ]; then
-    echo "y came"
+    echo "Setting Up Postgresql"
     cp -R -n /var/lib/postgresql/12/main_bkp/* /var/lib/postgresql/12/main
     chown -R postgres:postgres /var/lib/postgresql/12/main
     
@@ -14,13 +14,14 @@ fi
 chmod -R 700 /var/lib/postgresql/12/main
 
 z=$(eval "ls /var/lib/pgadmin | wc -l")
-echo "$z"
+#echo "$z"
 
 if [ "$z" -eq 0 ]; then
-    echo "z came"
+    echo "Setting Up PGAdmin"
     cp -R -n /var/lib/pgadmin_bkp/* /var/lib/pgadmin
     chown -R www-data:www-data /var/lib/pgadmin
 fi
 
 service postgresql start
 service apache2 start
+
